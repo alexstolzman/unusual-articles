85181c3b514e58e3e1c66e2b88262308f37ab4d7
alexstolzman <stolzmanalex@gmail.com>
2022-11-21 19:05:18 -0600

    Cleaned up code, changed the API call to get all articles to only occur once, and made the page look nicer

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,91 +4,68 @@ class Article extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            title: "Title",
+            title: "",
             imgUrl: "",
-            summary: "summary"
+            summary: "",
+            articles: []
         };
        
     }
 
+    //Get all articles immediately on component mount
+    componentDidMount(){
+        this.getAllArticles();
+    }
+
     //Get specific article
-    async getArticle(article){
-        //Replace spaces with underscores
-        // const formatted = article.replaceAll(' ', '_');
-        // const formatted2=formatted.replaceAll("'","%27")
-        //console.log(formatted2);
+    async getArticle(){
+        //Get random article from list of articles
+        const num=Math.floor(Math.random()*this.state.articles.length)
+        const article=this.state.articles[num].substring(18, this.state.articles[num].length-1)
+
         const response=await fetch("https://en.wikipedia.org/api/rest_v1/page/summary/"+article);
         const json=await response.json();
-        //Set summary and image url for page
-        this.setState({summary: json.extract, title: article})
 
+        //Set summary and image url for page
+        this.setState({summary: json.extract, title: json.title})
+
+        //Set image if it exists
         if(json.hasOwnProperty('originalimage')){
             this.setState({imgUrl: json.originalimage.source})
         }
-
-        
+        else{
+            this.setState({imgUrl: "No Image"})
+        }
     }
 
-    //Get all articles
-    async getArticles(){
-        //Fetch all table contents for the Unusual Articles page
-        //const response=await fetch("https://www.wikitable2json.com/api/Wikipedia%3AUnusual_articles?lang=en&cleanRef=false");
-        //const json=await response.json();
-
-        //Generate random numbers for category and articles in category
-        //const category=Math.floor(Math.random()*json.length)
-        //const article=Math.floor(Math.random()*json[category].length)
-
-        //this.setState({title: json[0][0][0]});
-        //console.log(json[0][0][0]);
-        //this.getArticle(json[category][article][0]);
-        //set state
-
-
-
-        //Get section, then use to get response
-        //https://en.wikipedia.org/w/api.php?action=parse&page=Wikipedia:Unusual_articles&prop=sections
-
-        const response2=await fetch("https://en.wikipedia.org/w/api.php?action=parse&origin=*&page=Wikipedia:Unusual_articles&prop=text&format=json",{
-            headers: {
-                'Content-Type': 'text/html'
-              }
-        }) //&prop=text
-        //<b><a href=\"/wiki/
-        //<b><a href=\\\"\/wiki\/
+    //Get all articles on the Unusual Articles Wikipedia page
+    async getAllArticles(){
+        const response2=await fetch("https://en.wikipedia.org/w/api.php?action=parse&origin=*&page=Wikipedia:Unusual_articles&prop=text&format=json") //&prop=text
         const json2=await response2.json();
-        //json2.split("<a=href\"").pop.split("\"");
-
+        
+        //Get article names from the text response
         let regex = new RegExp("<b><a href=\\\"\/wiki\/(.*?)\"", "g");
-
         const str=json2.parse.text['*'];
         let strArr=str.match(regex)
-        const num=Math.floor(Math.random()*strArr.length)
 
-        let title=strArr[num].substring(18, strArr[num].length-1)
+        this.setState({articles: strArr})
 
-        this.getArticle(title)
-        console.log(title)
-        //console.log(str.split("<b><a href=\\\"\/wiki\/")[0]);
 
 
     }
 
     handleClick=()=>{
-        this.getArticles();
+        this.getArticle();
     }
 
 
     render(){
-
-        //Add link to article
         return(
-            <div className='relative inset-y-5 px-4 py-4'>
-                
-                <h1>{this.state.title}</h1>
-                <img src={this.state.imgUrl} alt="Article image" style={{width:200, height:300}}/>
+            <div className='relative inset-y-5 px-4 py-4 object-center space-y-5'>
+                <a className="text-2xl font-bold text-stone-900" href={"https://en.wikipedia.org/wiki/"+this.state.title}>{this.state.title}</a>
+                <img className="mx-auto" src={this.state.imgUrl} alt="Article image" />
                 <p>{this.state.summary}</p>
-                <button onClick={this.handleClick}>Random Article</button>
+                <button className='bg-indigo-500  text-white' onClick={this.handleClick}>Random Article</button>
             </div>
         )
     }
